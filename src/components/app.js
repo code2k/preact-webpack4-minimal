1f48b8fb9b7321fb52edcb59c4d4dcf942dc29b5
Andreas Pataki <andreas.pataki@code2k.net>
2018-03-20 14:07:54 +0100

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import { h, Component } from "preact";
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div id="app">
+        Welcome to Preact-Webpack4-Minimal!
+      </div>
+    );
+  }
+
+}
